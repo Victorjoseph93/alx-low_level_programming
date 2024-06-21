48a23b123265e45748c557a4e4d821c7511cf1b5
Victorjoseph93 <victorjoseph001@gmail.com>
2024-06-21 10:29:48 -0700

    print base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - enter program input
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
