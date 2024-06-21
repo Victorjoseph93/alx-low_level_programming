2a25412a081676b109dc1242d99c62d1ca7024df
Victorjoseph93 <victorjoseph001@gmail.com>
2024-06-21 10:21:51 -0700

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - input
+ * Return: (0) successful
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
