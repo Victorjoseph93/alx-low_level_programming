a37699db3601a8590d2eb7837abdcb8ae306b0bb
Victorjoseph93 <victorjoseph001@gmail.com>
2024-06-21 08:25:36 -0700

    assign alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - input
+ * Return: (0) WHEN successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
