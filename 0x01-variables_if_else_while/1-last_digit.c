2037a684c9f8a381bf9554362da755e057dd488d
Victorjoseph93 <victorjoseph001@gmail.com>
2024-06-21 08:10:34 -0700

    assign number to variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if (j > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, j);
+		printf("Last digit of %d is %d and is greater than 5\n", n, j);
 	}
 	else if (j == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, j);
+		printf("Last digit of %d is %d and is 0\n", n, j);
 	}
 	else
 	{
-		printf("last digit of %d is %d and i less than 6 and not 0\n", n, j);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 	}
 	return (0);
 }
