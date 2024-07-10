4603ccfe0e3f868fa6fee8152b34feca7bbf99e2
Victorjoseph93 <victorjoseph001@gmail.com>
2024-07-10 05:47:52 -0700

    task 1 recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks the code function
+ * Return: always 0 when successful
+ *
+ */
+
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
