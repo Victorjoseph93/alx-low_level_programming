b6d764af7d50319dd5c0969778c3f66206a396bd
Victorjoseph93 <victorjoseph001@gmail.com>
2024-07-12 07:23:37 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - A program that Prints its name
+  * @argc: the argument count
+  * @argv: the argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+This README file describes the argc and argv project
+0x0A. C - argc, argv
