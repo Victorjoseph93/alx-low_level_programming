e70c1ef68770d9666991e47710f2209772ba2530
Victorjoseph93 <victorjoseph001@gmail.com>
2024-07-18 09:12:10 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,12 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int _isdigit(char *argv);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif /* MAIN_H */
