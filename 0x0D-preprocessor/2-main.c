1f8345625220852ee73fa193d3cd572033157f50
Victorjoseph93 <victorjoseph001@gmail.com>
2024-07-22 05:13:25 -0700

    task 2 preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - the function will print the name of file
+  * Return: always 0 when successful
+  */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return(0);
+}
